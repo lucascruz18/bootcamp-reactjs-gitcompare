1a7de285acf08c393b3afcea72aa04d8ffc4eda7
Lucas <lucas@alternativainternet.com>
2019-03-08 14:51:10 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+
+import Main from './pages/Main/index';
+
+const App = () => (
+  <div>
+    <GlobalStyles />
+    <Main />
+  </div>
+);
+
+export default App;
